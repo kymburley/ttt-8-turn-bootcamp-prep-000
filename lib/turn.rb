5534ea729a3957afc6bb250637c9f532481c22a9
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-11 17:32:33 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+  puts board
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  end
+end
+
+=begin
+def input_to_index(input)
+   input = input.to_i - 1
+end
+=end
+ 
+def position_taken?(board, index)
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+      false
+    elsif (board[index] == "X" || board[index] == "O")
+      true
+    end
+end
+
+def move(board, index, player = "X")
+  board[index] = player
+end
