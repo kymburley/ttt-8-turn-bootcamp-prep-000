fcf5cae3f06e48a118752d89bf6a26f0351571a5
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-11 17:38:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,16 @@
 def display_board(board)
-  puts board
+  cell_divider = "|"
+  row_divider = "-----------"
+ 
+  row1 = " #{board[0]} #{cell_divider} #{board[1]} #{cell_divider} #{board[2]} "
+  row2 = " #{board[3]} #{cell_divider} #{board[4]} #{cell_divider} #{board[5]} "
+  row3 = " #{board[6]} #{cell_divider} #{board[7]} #{cell_divider} #{board[8]} "
+ 
+  puts row1
+  puts row_divider
+  puts row2
+  puts row_divider
+  puts row3
 end
 
 def valid_move?(board, index)
